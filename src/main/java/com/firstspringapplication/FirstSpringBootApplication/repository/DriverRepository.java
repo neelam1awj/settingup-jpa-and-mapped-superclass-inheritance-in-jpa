1807538d7c9993ce61a7279a5d9bbbdb6e045830
neelam-6 <neelamawjhekar@gmail.com>
2026-02-08 23:11:06 +0530

    @Query: raw sql query and hibernate query

--- src/main/java/com/firstspringapplication/FirstSpringBootApplication/repository/DriverRepository.java
+++ src/main/java/com/firstspringapplication/FirstSpringBootApplication/repository/DriverRepository.java
@@ -22,6 +22,6 @@ public interface DriverRepository extends JpaRepository<Driver,Long> {
       //difference between raw and non-raw is that in raw we have to write the query by our self and in non raw and hibernate query language and hibernate will convert it to sql query and execute it and in raw we have to write the sql query by our self and execute it
 
 
-      @Query("SELECT Driver from Driver WHERE id = :id AND d.licenseNumber = :ln")
+      @Query("SELECT d FROM Driver d WHERE d.id = :id AND d.licenseNumber = :ln")
       Optional<Driver> HqlFindByIdAndLicense(Long id, String ln);
 }
